c05744b38e7fc8af61907bd12291983763253e87
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2022-12-21 22:11:17 +0530

    executeScript deprecation fixed

--- extension/background.js
+++ extension/background.js
@@ -1,103 +1,121 @@
- var socket = io.connect('http://localhost:4000')
-//var socket = io.connect('https://watchpartyserver.herokuapp.com/')
+console.log("Backgroup Script");
+
+import {io} from "./node_modules/socket.io-client";
+
+var socket = io.connect("http://localhost:4000");
+// //var socket = io.connect('https://watchpartyserver.herokuapp.com/')
 
 var existingConnection = false;
-var userData = {}
-var user_list = {}
-var chatData = []
+var userData = {};
+var user_list = {};
+var chatData = [];
 
-function checkStatus(){
-    if (socket.connected){
-        chrome.runtime.sendMessage({event:'socketStatus',data:true});
-    }else{
-        chrome.runtime.sendMessage({event:'socketStatus',data:false});
-    }    
+// checks status of socket Connection
+
+function checkStatus() {
+  console.log(socket.connected);
+  if (socket.connected) {
+    chrome.runtime.sendMessage({ event: "socketStatus", data: true });
+  } else {
+    chrome.runtime.sendMessage({ event: "socketStatus", data: false });
+  }
 }
 
-chrome.runtime.onMessage.addListener(function(message, sender, senderResponse){
-
-    if (message.event === "joinRoom"){
-        checkStatus()
-        if (existingConnection){
-            alert('You are already in a room')
-        }else{
-            existingConnection = true;
-            userData = {
-                username:message.data.username,
-                roomID:message.data.roomID
-            }
-            socket.emit('joinRoom',userData)
-        }
-        
-    }else if (message.event === "checkAlive"){
-        checkStatus()
-        if (existingConnection){
-            chrome.runtime.sendMessage({event:"checkAlive",data:{userData:userData,users:user_list}})
-        }else{
-            chrome.runtime.sendMessage({event:"checkAlive",data:''})
-        }
-
-
-    }else if (message.event === "syncVideo"){
-        socket.emit('syncVideo',[userData,message.data])
+chrome.runtime.onMessage.addListener(function (
+  message,
+  sender,
+  senderResponse
+) {
+  if (message.event === "joinRoom") {
+    checkStatus();
+    if (existingConnection) {
+      alert("You are already in a room");
+    } else {
+      existingConnection = true;
+      userData = {
+        username: message.data.username,
+        roomID: message.data.roomID,
+      };
+      socket.emit("joinRoom", userData);
     }
-
-    else if (message.event === "leaveRoom"){
-        socket.emit('leaveRoom',userData)
-        chatData = []
-        existingConnection = false;
+  } else if (message.event === "checkAlive") {
+    checkStatus();
+    if (existingConnection) {
+      chrome.runtime.sendMessage({
+        event: "checkAlive",
+        data: { userData: userData, users: user_list },
+      });
+    } else {
+      chrome.runtime.sendMessage({ event: "checkAlive", data: "" });
     }
+  } else if (message.event === "syncVideo") {
+    socket.emit("syncVideo", [userData, message.data]);
+  } else if (message.event === "leaveRoom") {
+    socket.emit("leaveRoom", userData);
+    chatData = [];
+    existingConnection = false;
+  } else if (message.event === "sendMessage") {
+    chatData.push({ username: userData.username, message: message.data });
+    socket.emit("sendMessage", { userData: userData, message: message.data });
+  } else if (message.event === "fetchMessages") {
+    chrome.runtime.sendMessage({ event: "sendMessage", data: chatData });
+  } else if (message.event === "setVideoState") {
+    chrome.scripting.executeScript(null, { file: "./getDuration.js" }, (data) => {
+      var time = data[0][0];
+      var isPaused = data[0][1];
+      socket.emit("syncVideo", [userData, [time, isPaused]]);
+    });
+  }
+});
 
-    else if (message.event === 'sendMessage'){
-        chatData.push({username:userData.username,message:message.data})
-        socket.emit('sendMessage',{userData:userData,message:message.data})
-    }
+// functions
 
-    else if(message.event === 'fetchMessages'){
-        chrome.runtime.sendMessage({event:'sendMessage',data:chatData});
-    }
+socket.on("joinRoom", (users) => {
+  chrome.runtime.sendMessage({
+    event: "joinRoom",
+    data: { userData: userData, users: users },
+  });
+  user_list = users;
+});
 
-    else if (message.event === 'setVideoState'){
-        chrome.tabs.executeScript(null,{file:'./getDuration.js'},(data) => {
-            var time = data[0][0]
-            var isPaused = data[0][1]
-            socket.emit('syncVideo',[userData,[time,isPaused]])
-        })
-    }
-})
+socket.on("leaveRoom", (users) => {
+  chrome.runtime.sendMessage({ event: "leaveRoom", data: users });
+});
 
+socket.on("syncVideo", (data) => {
+  duration = data[0];
+  isPaused = data[1];
+  function current(){
+    var videoElements=document.querySelectorAll('video')[0]; 
+    videoElements.currentTime = duration;
+  }
+  function pause(){
+    var videoElements=document.querySelectorAll('video')[0]; 
+    videoElements.pause();
+  }
+  function current(){
+    var videoElements=document.querySelectorAll('video')[0]; 
+    videoElements.play();
+  }
 
-socket.on('joinRoom',(users) =>{
-    chrome.runtime.sendMessage({event:'joinRoom',data:{userData:userData,users:users}});
-    user_list = users
-})
+  chrome.scripting.executeScript(null, {
+    function: current()
+  });
+  if (isPaused) {
+    chrome.scripting.executeScript(null, {
+      function: pause()
+    });
+  } else {
+    chrome.scripting.executeScript(null, {
+      function: play()
+    });
+  }
+});
 
-
-
-socket.on('leaveRoom',(users) => {
-    chrome.runtime.sendMessage({event:'leaveRoom',data:users});
-})
-
-
-socket.on('syncVideo',(data) => {
-    duration = data[0]
-    isPaused = data[1]
-    chrome.tabs.executeScript(null,{code:`var videoElements = document.querySelectorAll('video')[0]; videoElements.currentTime = ${duration}`})
-    if (isPaused){
-        chrome.tabs.executeScript(null,{code:`var videoElements = document.querySelectorAll('video')[0]; videoElements.pause()`})
-    }else{
-        chrome.tabs.executeScript(null,{code:`var videoElements = document.querySelectorAll('video')[0]; videoElements.play()`})
-    }
-
-})
-
-
-socket.on('sendMessage', (data) => {
-    username = data.username
-    message = data.message
-    console.log(`${username} says ${message}`)
-    chatData.push({username:username,message:message})
-    chrome.runtime.sendMessage({event:'sendMessage',data:chatData});
-})
-
-
+socket.on("sendMessage", (data) => {
+  username = data.username;
+  message = data.message;
+  console.log(`${username} says ${message}`);
+  chatData.push({ username: username, message: message });
+  chrome.runtime.sendMessage({ event: "sendMessage", data: chatData });
+});
