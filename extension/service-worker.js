d69d139fda7b3dd12d3e35c39c83f7717b44bbc5
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2023-01-02 03:12:04 +0530

    socket connections up and working

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,7 +1,7 @@
 //injecting socket.io scripts
-import './socket.io-client/dist/socket.io.js'
+import './socket.io.js'
 
 //importing background scripts
-import './background.js';
+// import './background.js';
 
 console.log("service worker script successfully injected");
