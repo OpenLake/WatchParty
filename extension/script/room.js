7db44aa8b9c40fa01997b34e57048f0451883957
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2023-01-14 01:16:32 +0530

    host span css added

--- extension/script/room.js
+++ extension/script/room.js
@@ -87,9 +87,9 @@ chrome.runtime.onMessage.addListener(function(message, sender, senderResponse){
         usersBox.innerHTML = '<br><ul class="list-group">'
         for (i = 0; i < users.length; ++i){
             if (users[i].username === userData.username){
-                usersBox.innerHTML += `<li class="list-group-item active"><span>${users[i].username}</span><span id="Host">check</span></li>`
+                usersBox.innerHTML += `<li class="list-group-item active"><span>${users[i].username}</span><span id="Host">H</span></li>`
             }else{
-                usersBox.innerHTML += `<li class="list-group-item"><span>${users[i].username}</span><span id="Host">check</span></li>`
+                usersBox.innerHTML += `<li class="list-group-item"><span>${users[i].username}</span><span id="Host">H</span></li>`
             }
         }
         usersBox.innerHTML += '</ul>'
@@ -105,9 +105,9 @@ chrome.runtime.onMessage.addListener(function(message, sender, senderResponse){
         users = message.data
         for (i = 0; i < users.length; ++i){
             if (users[i].username === userData.username){
-                usersBox.innerHTML += `<li class="list-group-item active"><span>${users[i].username}</span><span id="Host">check</span></li>`
+                usersBox.innerHTML += `<li class="list-group-item active"><span>${users[i].username}</span><span id="Host">H</span></li>`
             }else{
-                usersBox.innerHTML += `<li class="list-group-item"><span>${users[i].username}</span><span id="Host">check</span></li>`
+                usersBox.innerHTML += `<li class="list-group-item"><span>${users[i].username}</span><span id="Host">H</span></li>`
             }
             
 
